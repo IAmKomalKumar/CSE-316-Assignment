753c59736940969b8ac3e09bb649b26c4f825122
Deepak patel <37888688+deepu8837@users.noreply.github.com>
2019-04-17 21:58:04 +0530

    Update Problem 1.c

--- Problem 1.c
+++ Problem 1.c
@@ -1,46 +1,80 @@
+/* Design a scheduler with multilevel queue having two queues which will schedule the processes on 
+the basis of  preemptive shortest remaining processing time first algorithm (SROT) followed by
+a scheduling in which each process will get 2 units of time to execute. Also note that queue 1 
+has higher priority than queue 2.  Consider the following set of processes (for reference)with
+their arrival times and the CPU burst times in milliseconds.
+-------------------------------------
+Process  Arrival-Time   Burst-Time
+-------------------------------------
+P1             0      	       5
+P2             1               3
+P3             2               3
+P4             4               1
+-------------------------------------
+
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
+typedef struct
+{
+
+    int arrival_time,burst_time,queue;
+    int process_id;
+}process;
+
 int main()
 {
-int p[20],bt[20], su[20], wt[20],tat[20],i, k, n, temp;
-float wtavg, tatavg;
-printf("Enter the number of processes --- ");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-p[i] = i;
-printf("Enter the Burst Time of Process %d --- ", i);
-scanf("%d",&bt[i]);
-printf("Queue1/Queue2(0/1) ? --- ");
-scanf("%d", &su[i]);
-}
-for(i=0;i<n;i++)
-for(k=i+1;k<n;k++)
-if(su[i] > su[k])
-{
-temp=p[i];
-p[i]=p[k];
-p[k]=temp;
-temp=bt[i];
-bt[i]=bt[k];
-bt[k]=temp;
-temp=su[i];
-su[i]=su[k];
-su[k]=temp;
-}
-wtavg = wt[0] = 0;
-tatavg = tat[0] = bt[0];
-for(i=1;i<n;i++)
-{
-wt[i] = wt[i-1] + bt[i-1];
-tat[i] = tat[i-1] + bt[i];
-wtavg = wtavg + wt[i];
-tatavg = tatavg + tat[i];
-}
-printf("\nPROCESS\t\t SYSTEM/USER PROCESS \t   BURST TIME\t    WAITING TIME\t    TURNAROUND TIME");
-for(i=0;i<n;i++)
-printf("\n%d \t\t\t %d \t\t\t %d \t\t %d \t\t   %d ",p[i],su[i],bt[i],wt[i],tat[i]);
-printf("\nAverage Waiting Time is --- %f",wtavg/n);
-printf("\nAverage Turnaround Time is --- %f",tatavg/n);
-getch();
+    int size;//Number of processes
+    int count = 0;
+    int time,smallest,waiting_time[50],turnaround_time[50],completion[50],temp_burst[50];
+    double average_waiting,average_turnaround;
+    printf("\nEnter the number of processes:");
+    scanf("%d",&size);
+    process no[size];
+    for(int i = 0; i < size; i++)
+	{
+		printf("\nProcess ID:\t");
+		scanf("%d", &no[i].process_id);
+		printf("Arrival Time:\t");
+		scanf("%d", &no[i].arrival_time);
+		printf("Burst Time:\t");
+		scanf("%d", &no[i].burst_time);
+		printf("Queue1/Queue2(1/2):\t");
+		scanf("%d", &no[i].queue);
+	}
+    for(int i=0;i<size;i++)
+    {
+        temp_burst[i] = no[i].burst_time;//storing the burst time in a array for  further use.
+    }
+    no[size+1].burst_time = 999;//  assign a max burst for comparison
+    for(time=0;count!=size;time++)
+    {
+         smallest = size+1;
+         for(int i=0;i<size;i++)
+         {
+            if(no[i].arrival_time<=time && no[i].burst_time<no[smallest].burst_time&& no[i].burst_time>0 )
+            smallest=i;
+         }
+         no[smallest].burst_time--;
+         if(no[smallest].burst_time == 0)
+         {
+            count++;
+            completion[smallest] = time+1;
+            turnaround_time[smallest] = completion[smallest] - no[smallest].arrival_time;
+            waiting_time[smallest] = turnaround_time[smallest] - temp_burst[smallest];
+         }
+    }
+    printf("\n\nProcess Id\tArrival Time\t Burst Time\t Waiting Time\tTurnaround Time");
+    for(int i=0;i<size;i++)
+    {
+        printf("\n   %d    \t\t%d\t\t%d  \t\t%d\t\t%d",no[i].process_id,no[i].arrival_time,temp_burst[i],waiting_time[i],turnaround_time[i]);
+        average_waiting += waiting_time[i];
+        average_turnaround += turnaround_time[i];
+    }
+    printf("\n\nAverage waiting time = %lf\n",average_waiting/size);
+    printf("Average Turnaround time = %lf",average_turnaround/size);
+    getch();
+
 }
